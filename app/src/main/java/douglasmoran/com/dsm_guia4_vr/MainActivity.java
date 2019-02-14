7065123cf5fe441a5914261cd88a5f1019fc28d4
Douglas Moran <douglasflores9817@gmail.com>
2019-02-14 17:20:27 -0600

    Iniciando proyect

---
+++ app/src/main/java/douglasmoran/com/dsm_guia4_vr/MainActivity.java
@@ -0,0 +1,13 @@
+package douglasmoran.com.dsm_guia4_vr;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
