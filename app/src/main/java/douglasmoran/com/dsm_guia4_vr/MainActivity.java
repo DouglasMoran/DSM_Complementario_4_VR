d667638823b04ffa839238af5d2a811e9ccdb653
Douglas Moran <douglasflores9817@gmail.com>
2019-02-26 08:35:41 -0600

    add img UDB

--- app/src/main/java/douglasmoran/com/dsm_guia4_vr/MainActivity.java
+++ app/src/main/java/douglasmoran/com/dsm_guia4_vr/MainActivity.java
@@ -28,6 +28,10 @@ public class MainActivity extends AppCompatActivity {
         images.add(new Images(3,R.drawable.img3));
         images.add(new Images(4,R.drawable.img4));
         images.add(new Images(5,R.drawable.img5));
+        images.add(new Images(5,R.drawable.imgvruno));
+        images.add(new Images(5,R.drawable.imgvrdos));
+        images.add(new Images(5,R.drawable.imgvrtres));
+
 
 
         RecyclerView recyclerView = findViewById(R.id.recyclerImages);
